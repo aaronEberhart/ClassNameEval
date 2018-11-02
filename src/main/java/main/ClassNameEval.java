ce2ca01c2e1820c83f35082ff369b9a27b9fd38a
aaron <aaron@aaron-Aspire-R5-471T>
2018-11-01 22:21:18 -0400

    hfs

--- src/main/java/main/ClassNameEval.java
+++ src/main/java/main/ClassNameEval.java
@@ -9,7 +9,7 @@ import util.TextFileOntologySplicer;
 public class ClassNameEval {
 	
 	public static void main(String[] args) {
-		TextFileOntologySplicer fileOnt = new TextFileOntologySplicer("OWLs/answers.txt","OWLs/ComputationalEnvironment_modified_.owl");
+		TextFileOntologySplicer fileOnt = new TextFileOntologySplicer("OWLs/answers.txt","OWLs/ComputationalEnvironment_modified.owl");
 		Ontology onto = fileOnt.getOntology();
 		OntologyParser parser = new OntologyParser(onto);
 		List<List<String>> s;
--- src/main/java/util/TextFileOntologySplicer.java
+++ src/main/java/util/TextFileOntologySplicer.java
@@ -8,8 +8,19 @@ import ontologyStuff.*;
 public class TextFileOntologySplicer {
 	
 	private Ontology ontology;
-	private ArrayList<ArrayList<String[]>> fileData;
-	private ArrayList<ArrayList<Integer>> stats;
+	
+	private ArrayList<Duple<String,ArrayList<ArrayList<String[]>>>> fileData;
+	private ArrayList<Duple<String,ArrayList<Duple<String,Integer>>>> stats;
+	
+	private class Duple<X,Y>{
+		public X x;
+		public Y y;
+		public Duple() {}
+		public Duple(X x, Y y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
 	
 	public TextFileOntologySplicer(String textFilename, String ontologyFilename) {
 		
@@ -34,34 +45,49 @@ public class TextFileOntologySplicer {
 	}
 	
 	private void readFile(String filename) {
-		fileData = new ArrayList<ArrayList<String[]>>();
+		fileData = new ArrayList<Duple<String,ArrayList<ArrayList<String[]>>>>();
 		
 		try {
 			BufferedReader reader = new BufferedReader(new FileReader(filename));
 			
-			ArrayList<String[]> lines = new ArrayList<String[]>();
+			Duple<String,ArrayList<ArrayList<String[]>>> type = new Duple<String,ArrayList<ArrayList<String[]>>>();
+			ArrayList<ArrayList<String[]>> tuples = new ArrayList<ArrayList<String[]>>();
+			ArrayList<String[]> entry = new ArrayList<String[]>();
 			String[] tmp;
+			String current = "";
 			
 			while(reader.ready()) {				
 				
 				String line = reader.readLine();
 								
 				if(line.matches("^-.*")) {
-					tmp = new String[2];
-					String id = line.split("-")[1];
-					tmp[0] = id.substring(0,3);
-					tmp[1] = id.substring(3);
-					lines.add(tmp);
+					tmp = new String[1];
+					String id = (line.split("-")[1]).substring(0,3);
+					if(current.equals("")) {
+						current = id;
+						type = new Duple<String,ArrayList<ArrayList<String[]>>>();
+						type.x = id;
+					}else if(!current.equals(id)){
+						type.y = tuples;
+						fileData.add(type);
+						type = new Duple<String,ArrayList<ArrayList<String[]>>>();
+						tuples = new ArrayList<ArrayList<String[]>>();
+						type.x = id;
+						current = id;
+					}
 				}else if(line.matches("")) {
-					fileData.add(lines);
-					lines = new ArrayList<String[]>();
+					tuples.add(entry);
+					entry = new ArrayList<String[]>();
 				}else {
 					tmp = new String[2];
 					tmp = line.split(": ");
-					lines.add(tmp);
+					entry.add(tmp);
 				}
 				
 			}
+			
+			type.y = tuples;
+			fileData.add(type);
 			
 			reader.close();
 			
@@ -74,7 +100,18 @@ public class TextFileOntologySplicer {
 	
 
 	private void loadStats() {
+		Duple<String,ArrayList<Duple<String,Integer>>> all = new Duple<String,ArrayList<Duple<String,Integer>>>();
+		all.x = "all";
+		stats.add(all);
 		
+		for(Duple<String,ArrayList<ArrayList<String[]>>> type : fileData) {
+			Duple<String,ArrayList<Duple<String,Integer>>> typeCounts = new Duple<String,ArrayList<Duple<String,Integer>>>();
+			ArrayList<Duple<String,Integer>> counts = new ArrayList<Duple<String,Integer>>();
+			typeCounts.x = type.x;
+			for(ArrayList<String[]> entry : type.y) {
+				
+			}
+		}
 	}
 	
 	public Ontology getOntology() {
